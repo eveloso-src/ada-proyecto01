e33846791b08544bffcc0c049a4a810806dc9f88
eveloso-src <elemudela@gmail.com>
2019-11-02 09:57:48 -0300

    Add files via upload

---
+++ proyectos/src/main/java/proyecto/ClaseComentarios.java
@@ -0,0 +1,21 @@
+package proyecto;
+
+
+/**
+ * Esta clase se encarga de describir los comentarios
+ * 
+ * @author GOD
+ *
+ */
+public class ClaseComentarios {
+
+	
+	private String nombre;
+	
+	/**
+	 * Javadoc para el metodo
+	 */
+	public void metodoPrueba() {
+		
+	}
+}
